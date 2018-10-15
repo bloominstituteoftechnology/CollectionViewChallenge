64e40b42b9f9e0c07967a2ce6a2b2f6638951b16
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-15 14:05:32 -0700

    loaded images and comformed collection delegate

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,5 +1,23 @@
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    let reuseIdentifier = "cell"
+    
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        for number in 1...12 {
+        guard let image = UIImage(named: "Image\(number)") else { return }
+        images.append(image)
+        }
+    }
+    
+    collectionview
 }
+
