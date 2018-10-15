7c1b0104f8823436bf99798af91a7541a871fec4
OmegaWulf <nikcxz@live.com>
2018-10-15 15:31:58 -0400

    Initial Commit

---
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewChallenge
+//
+//  Created by Nikita Thomas on 10/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var imageView: UIImageView!
+    
+}
---
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewChallenge
+//
+//  Created by Nikita Thomas on 10/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+    
+}
