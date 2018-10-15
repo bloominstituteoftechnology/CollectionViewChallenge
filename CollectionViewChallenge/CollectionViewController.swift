785f807f0d9b8ee95e0daf7c7889eb29abab8d4b
Sean Doyle <sdoylelambda@gmail.com>
2018-10-15 17:02:50 -0400

    most code in, need to fix errors

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     let reuseIdentifier = "cell"
     var images: [UIImage] = []
@@ -9,6 +9,7 @@ class CollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        
         for i in 1...12 {
             guard let image = UIImage(named: "Image\(i)") else { return }
             //let painting = UIImage(named: "Image\(i)")
@@ -17,16 +18,47 @@ class CollectionViewController: UICollectionViewController {
         }
     
     let targetDimension: CGFloat = 320
-    let insetAmount: CGFloat = 32
-    
+    //let insetAmount: CGFloat = 32
+    let maxDimension = 1000
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {fatalError("unable to retrieve layout")}
         
+        let insetAmount: CGFloat = 32
+        
         layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
         layout.itemSize = CGSize(width: 80, height: 80)
         layout.headerReferenceSize = CGSize(width: collectionView.bounds.width, height: 32)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+        }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell else {fatalError("No Cell")}
+        cell.imageView.image = images[indexPath.row]
+        return cell
+    }
+
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath)  -> CGSize {
+        let image = images[indexPath.row]
+        let maxDiminsion = max(image.size.width, image.size.height)
+      
+   
+    
+
+        let scale = targetDimension / maxDimension
+        
+         return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+//    }
+//
+//    override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+//        guard let cell = collectionView.cellForItem(at: indexPath) else {return}
+//        cell.layer.borderWidth = 1
+//        cell.layer.borderColor = UIColor.yellow.cgColor
+    }
+
 }
 
