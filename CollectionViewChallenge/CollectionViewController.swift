4a9ab361567c4830fa335a51f9ec032b58707c61
OmegaWulf <nikcxz@live.com>
2018-10-15 15:52:51 -0400

    Controller setup complete

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -10,5 +10,48 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    var reuseIdentifier = "cell"
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        for i in 1...12 {
+            guard let image = UIImage(named: "image\(i)") else {return}
+            images.append(image)
+        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {fatalError("Unable to retrieve layout")}
+        
+        let targetDimension: CGFloat = 320
+        let insetAmount: CGFloat = 32
+        
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+        layout.minimumLineSpacing = .greatestFiniteMagnitude
+        
+        layout.headerReferenceSize = CGSize(width: collectionView.bounds.width, height: 32)
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell else {fatalError("no cell. oops")}
+        
+        cell.imageView.image = images[indexPath.row]
+        return cell
+    }
+    
+    
+    
     
 }
