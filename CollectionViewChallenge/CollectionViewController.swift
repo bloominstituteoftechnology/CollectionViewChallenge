f45a91d88bf8981a56bd225814d93f8c6c6b0750
yzhukovs <yzhukovs@bumnetworks.com>
2018-10-15 18:26:50 -0400

    indentation

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -10,12 +10,12 @@ import UIKit
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     let reuseIndetifier = "cell"
-     var images: [UIImage] = []
+    var images: [UIImage] = []
     
     override func viewDidLoad() {
         let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: reuseIndetifier)
-    
+        
         for i in 1...12 {
             let image: String = "Image\(i)"
             guard UIImage(named: image) != nil else {return}
@@ -23,32 +23,35 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
             images.append(painting!)
         }
         
-    
+        
     }
-
-let targetDimension: CGFloat = 320
+    
+    let targetDimension: CGFloat = 320
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
             fatalError("Unable to retrieve a layout")
         }
-
+        
         let insetAmount: CGFloat = 32
-layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
-       layout.minimumLineSpacing = .greatestFiniteMagnitude
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+        layout.minimumLineSpacing = .greatestFiniteMagnitude
         layout.scrollDirection = .horizontal
         
-
-    
         
-}
-
+        
+        
+    }
+    
+    
+    
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
         return images.count
         
     }
-
+    
     
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
@@ -57,12 +60,14 @@ layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: 
             fatalError("Inconsistent view state")
         }
         
-    cell.imageVIew.image = self.images[indexPath.row]
+        cell.imageVIew.image = self.images[indexPath.row]
         
         
+        cell.isSelected = true
         
-//        cell.nameLabel.text = ColorHelper.shared.nameFor(indexPath: indexPath)
-       cell.layer.borderColor = UIColor.yellow.cgColor
+        
+        //cell.backgroundColor = UIColor.yellow.cgColor
+        cell.layer.borderColor = UIColor.yellow.cgColor
         cell.layer.borderWidth = 1
         
         return cell
@@ -82,8 +87,11 @@ layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: 
         // Calculate how to scale that largest dimension into `targetDimension`
         let scale = targetDimension / maxDimension
         
+        
         // Return scaled dimensions
         return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        
+        
     }
     
     
