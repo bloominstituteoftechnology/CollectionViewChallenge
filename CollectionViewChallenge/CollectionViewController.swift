080fb2e40a4df4d7360a9b6af24ed6976d980c22
Moses Robinson <mosesjrobinson@icloud.com>
2018-10-15 16:02:43 -0700

    completed project corrected number of section problem

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -14,10 +14,57 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         
         for number in 1...12 {
         guard let image = UIImage(named: "Image\(number)") else { return }
+            
         images.append(image)
+            
         }
     }
     
-    collectionview
+    let targetDimension: CGFloat = 320
+    let insetAmount: CGFloat = 32
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            fatalError("NO LAYOUT!!!!")
+        }
+        
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+        layout.minimumLineSpacing = .greatestFiniteMagnitude
+        layout.scrollDirection = .horizontal
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as? CollectionViewCell else {
+            fatalError("NO CELL!!!!!!")
+        }
+        
+        cell.imageView.image = images[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Fetch image
+        let image = images[indexPath.row]
+        
+        // Fetch largest dimension of the image, whether width or height
+        let maxDimension = max(image.size.width, image.size.height)
+        
+        // Calculate how to scale that largest dimension into `targetDimension`
+        let scale = targetDimension / maxDimension
+        
+        // Return scaled dimensions
+        return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+    }
 }
 
