24756cb072e3bdeece2c8db42814fe874890ad74
Paul Yi <yipaulc@gmail.com>
2018-10-15 15:14:40 -0700

    Load the images

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var images: [UIImage] = []
     
@@ -9,5 +9,30 @@ class CollectionViewController: UICollectionViewController {
     override func viewDidLoad() {
         let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
         collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        
+        for numbers in 1...12 {
+            let imageString = ("Image\(numbers)")
+            guard let image = UIImage(named: imageString) else { return }
+            images.append(image)
+        }
+    }
+
+    
+    override func viewWillAppear(_ animated: Bool) {
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            fatalError("Unable to retrieve layout")
+        }
+        
+        let targetDimension: CGFloat = 320
+        let insetAmount: CGFloat = 32
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+        layout.minimumLineSpacing = .greatestFiniteMagnitude
+        layout.scrollDirection = .vertical
+        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
     }
 }
