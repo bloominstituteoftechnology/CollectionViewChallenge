bb4747cc1952e6a3a4c8ef19dca85b730c1202cc
Paul Yi <yipaulc@gmail.com>
2018-10-15 12:55:45 -0700

    Set up the view cell

--- CollectionViewChallenge/CollectionViewCell.swift
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -2,5 +2,5 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
-    
+    @IBOutlet var imageView: UIImageView!
 }
--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -2,5 +2,12 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    var images: [UIImage] = []
     
+    let reuseIdentifier = "cell"
+    
+    override func viewDidLoad() {
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+    }
 }
