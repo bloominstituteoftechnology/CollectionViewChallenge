bb7297bc25e6dbd0f741576e89a78baf8027558f
Jerrick Warren <jerrickwarren@MacBook-Pro-3.local>
2018-10-15 14:11:00 -0700

    Initial Project Commit

---
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewChallenge
+//
+//  Created by Jerrick Warren on 10/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+}
