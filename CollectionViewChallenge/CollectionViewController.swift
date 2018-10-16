59d769ef9648e8f4b89234fe349e2eddacf2f62c
Rwoltx <richardx1016@gmail.com>
2018-10-15 21:31:18 -0400

    Initial commit

---
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewChallenge
+//
+//  Created by Rick Wolter on 10/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
