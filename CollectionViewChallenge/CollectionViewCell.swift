f7d0bb6661d554b35d7cb2de99f1981db9372ff4
Paul Yi <yipaulc@gmail.com>
2018-10-15 23:34:15 -0700

    Override isSelected property to provide yellow border on selected image

--- CollectionViewChallenge/CollectionViewCell.swift
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -4,4 +4,18 @@ class CollectionViewCell: UICollectionViewCell {
     
     @IBOutlet var imageView: UIImageView!
     
+    override var isSelected: Bool{
+        didSet{
+            if self.isSelected
+            {
+                self.layer.borderWidth = 10
+                self.layer.borderColor = UIColor.yellow.cgColor
+            }
+            else
+            {
+                self.layer.borderWidth = 0
+            }
+        }
+    }
+    
 }
