a53c9783c6a6fe09bd2c7815174a5d53c3552207
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-15 14:25:17 -0800

    set up an array in the CVC to load the images in the assets folder

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for counter in 1 ... 12 {
+            let paintingName = "Image\(counter)"
+            // if let image = UIImage(named: paintingName) {
+            guard let image = UIImage(named: paintingName) else { fatalError("Could not connect to image assets.") }
+            images.append(image)
+        }
+        // return images ?
+    }
+    
 }
