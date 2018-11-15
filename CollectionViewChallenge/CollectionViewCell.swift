e7699664d89837d079865bf65359ad9ef739183d
Ben Hakes <bhhakes@gmail.com>
2018-11-15 15:33:02 -0600

    Create CollectionViewController.swift, CollectionViewCell.swift, CollectionViewCell.xib

---
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewChallenge
+//
+//  Created by Benjamin Hakes on 11/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell{
+    
+}
