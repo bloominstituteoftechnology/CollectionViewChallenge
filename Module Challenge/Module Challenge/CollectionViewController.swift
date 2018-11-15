0ecfd6718fe3dbe13649e35112ba44b03525827a
Sergey Osipyan <osipyansergey@gmail.com>
2018-11-15 14:00:16 -0800

    working on error

--- Module Challenge/Module Challenge/CollectionViewController.swift
+++ Module Challenge/Module Challenge/CollectionViewController.swift
@@ -2,23 +2,54 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
+    
     var images: [UIImage] = []
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        guard let image = UIImage(named: name) else {return fatalError("Image name not exist")}
+    ????????????
+        guard let image = UIImage(named: name) else {return fatalError("Image name not exist") }
+        
+    
+    
+    
+        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath) as? CollectionViewCell else { fatalError("Error")}
+                
+            }
+    
+            override func viewDidLoad() {
+                super.viewDidLoad()
+                reloadInputViews()
+                
+            guard let layout = collectionViewLayout as? UICollectionViewFlowLayout else {
+                fatalError("Unable to retrieve layout")}
+        
         
         let targetDimension: CGFloat = 320
         let insetAmount: CGFloat = 32
         
-      func viewWillAppear(_ animated: Bool) {
-            collectionView.reloadData()
-            
-            
-        layout.sectionInset = UIEdgeInsets(top: amount, left: amount, bottom: amount, right: amount)
-        layout.itemSize = CGSize(width: 80, height: 80)
-        layout.headerReferenceSize = CGSize(width: collectionView.bounds.width, height: 32)
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+            layout.minimumLineSpacing = .greatestFiniteMagnitude
+            layout.scrollDirection = .horizontal
+       
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
+        // Fetch image
+        let image = images[indexPath.row]
+        
+        // Fetch largest dimension of the image, whether width or height
+        let maxDimension = max(image.size.width, image.size.height)
+        
+        // Calculate how to scale that largest dimension into `targetDimension`
+        let scale = targetDimension / maxDimension
+        
+        // Return scaled dimensions
+        return CGSize(width: image.size.width * scale, height: image.size.height * scale)
     }
-    
-}
+    }
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+    }
+
+
+
