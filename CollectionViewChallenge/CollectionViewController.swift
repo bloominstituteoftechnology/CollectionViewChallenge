dc379c62a2d9a35bb374d62d0dcb7146ad428890
Ben Hakes <bhhakes@gmail.com>
2018-11-15 16:43:49 -0600

    update datasource, animations, and dependencies for CollectionVC

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -8,10 +8,13 @@
 
 import UIKit
 
-class CollectionViewController: UICollectionViewController{
-    var images: [UIImage] = []
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout{
     
-    override func viewWillAppear(_ animated: Bool) {
+    var images: [UIImage] = []
+    let targetDimension: CGFloat = 320
+    let insetAmount: CGFloat = 32
+    
+    override func viewDidLoad() {
         super.viewDidLoad()
         
         for index in 1...12 {
@@ -20,8 +23,46 @@ class CollectionViewController: UICollectionViewController{
         }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let collectionViewLayout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {fatalError("missed assignment of collection view layout")}
+        
+        collectionViewLayout.sectionInset.bottom = insetAmount
+        collectionViewLayout.sectionInset.left = insetAmount
+        collectionViewLayout.sectionInset.right = insetAmount
+        collectionViewLayout.sectionInset.top = insetAmount
+        
+        collectionViewLayout.minimumLineSpacing = .greatestFiniteMagnitude
+        
+        collectionViewLayout.scrollDirection = .horizontal
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath) as? CollectionViewCell else {fatalError("unable to dequeue cell of proper type")}
+        
+        cell.imageView.image = images[indexPath.row]
+        
+        return cell
+    }
     
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        // Fetch image
+        let image = images[indexPath.row]
+        
+        // Fetch largest dimension of the image, whether width or height
+        let maxDimension = max(image.size.width, image.size.height)
+        
+        // Calculate how to scale that largest dimension into `targetDimension`
+        let scale = targetDimension / maxDimension
+        
+        // Return scaled dimensions
+        return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+    }
 }
