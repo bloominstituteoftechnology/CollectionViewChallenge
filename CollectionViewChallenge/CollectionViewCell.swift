a3ae335a5697220733dc9fcfb7e40705c0cc9c5e
IvanCaldwell <grumpy.caldwell@gmail.com>
2018-11-15 13:51:59 -0800

    Project Setup: Completed

---
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewChallenge
+//
+//  Created by Ivan Caldwell on 11/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ CollectionViewChallenge/CollectionViewCoontroller.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionViewCoontroller.swift
+//  CollectionViewChallenge
+//
+//  Created by Ivan Caldwell on 11/15/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+}
