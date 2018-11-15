0e489fccc5a2c812ec9525e25f6e037f20915207
Frulwinn <frulwinn@gmail.com>
2018-11-15 14:09:08 -0800

    set up for images to load using array in collection view

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,5 +1,18 @@
 import UIKit
 
 class CollectionViewController: UICollectionViewController {
+    let reuseIdentifier = "cell"
+    var images: [UIImage] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let nib = UINib(nibName: "CollectionViewCell", bundle: nil)
+        collectionView.register(nib, forCellWithReuseIdentifier: reuseIdentifier)
+        
+        for number in 1...12{
+            guard let image = UIImage(named: "Image\(number)") else { return }
+            images.append(image)
+        }
+    }
 }
