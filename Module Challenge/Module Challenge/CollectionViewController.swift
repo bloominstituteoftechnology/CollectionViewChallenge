8496535e794880231e5229deaadcafaa58069754
Sergey Osipyan <osipyansergey@gmail.com>
2018-11-15 14:40:34 -0800

    loading black screan and no error

--- Module Challenge/Module Challenge/CollectionViewController.swift
+++ Module Challenge/Module Challenge/CollectionViewController.swift
@@ -1,22 +1,24 @@
 import UIKit
 
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
-    
-    
+  
     var images: [UIImage] = []
-    
-    ????????????
-        guard let image = UIImage(named: name) else {return fatalError("Image name not exist") }
+    override func viewDidLoad() {
+     super.viewDidLoad()
         
+    for number in 1...12 {
+    let name = "Image\(number)"
+        guard let image = UIImage(named: name) else {fatalError("Image not exist")}
+        
+    images.append(image)
+        return
+        }
     
-    
-    
-        override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+         func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: CollectionViewCell.reuseIdentifier, for: indexPath) as? CollectionViewCell else { fatalError("Error")}
-                
-            }
-    
-            override func viewDidLoad() {
+            return cell
+        }
+             func viewDidLoad() {
                 super.viewDidLoad()
                 reloadInputViews()
                 
@@ -31,6 +33,7 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
             layout.minimumLineSpacing = .greatestFiniteMagnitude
             layout.scrollDirection = .horizontal
        
+                
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         
         // Fetch image
@@ -43,13 +46,11 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
         let scale = targetDimension / maxDimension
         
         // Return scaled dimensions
-        return CGSize(width: image.size.width * scale, height: image.size.height * scale)
+        return CGSize(width: image.size.width * scale, height: image.size.height * scale) }
     }
+     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count }
     }
-    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return images.count
-    }
-    }
-
+}
 
 
