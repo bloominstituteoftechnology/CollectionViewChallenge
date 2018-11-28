c01aee5594411ffd73a93189d80e9f0488bb8936
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-28 13:51:34 -0700

    Created empty array of images and then loaded them in the for-loop.

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,7 +1,16 @@
 import UIKit
 import Foundation
 
-class CollectionViewController {
+class CollectionViewController: UICollectionViewController {
     
+    var images: [UIImage] = []
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for values in 1...12 {
+            guard let image = UIImage(named: "Image\(values)") else { fatalError("Unable to retrieve images") }
+            images.append(image)
+        }
+    }
 }
