bbcf1cc57dbd50a1cdcfa9c5d420bb4a1c486b0f
Frulwinn <frulwinn@gmail.com>
2018-11-28 13:14:18 -0800

    added collectionviewcontroller, collectionviewcell, collectionviewxib

---
+++ Collection/Collection/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Collection
+//
+//  Created by Lambda_School_Loaner_34 on 11/28/18.
+//  Copyright © 2018 Frulwinn. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
