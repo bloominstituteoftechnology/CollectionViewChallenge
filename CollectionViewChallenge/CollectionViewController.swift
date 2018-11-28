5227b3044e3dbaaf5eb25c544c9042a1d654308b
arielmclaughlinn <arielmclaughlin@icloud.com>
2018-11-28 14:06:05 -0700

    Implemented viewWillAppear and casted the layout.

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -1,7 +1,7 @@
 import UIKit
 import Foundation
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var images: [UIImage] = []
     
@@ -9,8 +9,17 @@ class CollectionViewController: UICollectionViewController {
         super.viewDidLoad()
         
         for values in 1...12 {
-            guard let image = UIImage(named: "Image\(values)") else { fatalError("Unable to retrieve images") }
+            guard let image = UIImage(named: "Image\(values)") else { fatalError("Unable to load the images") }
             images.append(image)
         }
     }
+    
+    let targetDimension: CGFloat = 320
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else { fatalError("Unable to retrieve the layout.") }
+    }
+    let insetAmount: CGFloat = 32
 }
