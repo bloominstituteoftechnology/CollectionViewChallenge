235ad859f2aa043a2c5782242e958f86af05f2fa
Stuart Sippel <stuartsippel@icloud.com>
2018-11-28 15:19:26 -0600

    add CVC file, cell controller file, and .xib

---
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewController.swift
+//  CollectionViewChallenge
+//
+//  Created by Stuart on 11/28/18.
+//  Copyright © 2018 Erica Sadun. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewController: UICollectionViewController {
+    
+}
