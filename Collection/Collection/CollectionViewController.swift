98aced3e48d7c6a813afb4d5c68ba516b6923adb
Frulwinn <frulwinn@gmail.com>
2018-11-28 13:57:47 -0800

    created loop and string interpolation for image

--- Collection/Collection/CollectionViewController.swift
+++ Collection/Collection/CollectionViewController.swift
@@ -2,5 +2,14 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
 
-   
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for number in 1 ... 12 {
+            guard let image = UIImage(named: "Image\(number)") else { return }
+            images.append(image)
+        }
+    }
 }
