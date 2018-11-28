00e126d26df942cb12ab14f5b194329cecc19192
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2018-11-28 16:18:38 -0600

    12 images that will be displayed are loaded

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -2,4 +2,27 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    // Array that will hold the images
+    var images: [UIImage] = []
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 12 images into the image array
+        for counter in 1 ... 12 {
+            
+            // Name the images using string interpolation
+            let imageName = "image\(counter)"
+            
+            // If the image is there, create a UIImage of the named image
+            guard let image = UIImage(named: imageName) else { return }
+            
+            // Add the images to the images array
+            images.append(image)
+        }
+    }
+    
+    
+    
 }
