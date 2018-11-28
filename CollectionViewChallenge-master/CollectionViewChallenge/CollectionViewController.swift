c5313c11d16c194f05dfcfc6ab84b08159ecd886
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-28 13:26:51 -0800

    load the images
    this is probably set up incorrectly, so I'll fix it later.

--- CollectionViewChallenge-master/CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge-master/CollectionViewChallenge/CollectionViewController.swift
@@ -2,4 +2,13 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    override func viewDidLoad() {
+        var nameArray: [UIImage] = []
+        for counter in 1 ... 12 {
+            let imageName = "image\(counter)"
+            guard let image = UIImage(named: imageName) else { fatalError("No images") }
+                nameArray.append(image)
+            }
+    }
+    
 }
