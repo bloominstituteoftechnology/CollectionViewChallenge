dbe3e27b4c4c4f28fc6c04df274bf42647bf170e
Frulwinn <frulwinn@gmail.com>
2018-11-28 13:17:54 -0800

    added collection view controller to IB

--- Collection/Collection/ViewController.swift
+++ Collection/Collection/ViewController.swift
@@ -1,20 +1,6 @@
-//
-//  ViewController.swift
-//  Collection
-//
-//  Created by Lambda_School_Loaner_34 on 11/28/18.
-//  Copyright © 2018 Frulwinn. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
 
 }
 
