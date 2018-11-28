c97619258840cb87d2d3ac99d3f99e0098023aa1
Vijay Das <vijaytdas@gmail.com>
2018-11-28 16:12:16 -0600

    add load function to CollectionViewController

--- CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge/CollectionViewController.swift
@@ -2,4 +2,18 @@ import UIKit
 
 class CollectionViewController: UICollectionViewController {
     
+    var images: [UIImage] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for counter in 1 ... 12 {
+            let name = "image\(counter)"
+            guard let image = UIImage(named: name) else { return }
+            images.append(image)
+            
+        }
+       
+     }
+    
 }
