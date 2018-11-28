b2af3b2ad438834a57777d8aac1cbdae726e16af
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-28 13:54:15 -0800

    set up the layout

--- CollectionViewChallenge-master/CollectionViewChallenge/CollectionViewController.swift
+++ CollectionViewChallenge-master/CollectionViewChallenge/CollectionViewController.swift
@@ -1,8 +1,10 @@
 import UIKit
 
-class CollectionViewController: UICollectionViewController {
+class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
     
     var paintings: [UIImage] = []
+    let targetDimension: CGFloat = 320
+    let insetAmount: CGFloat = 32
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -14,4 +16,16 @@ class CollectionViewController: UICollectionViewController {
             }
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    
+        guard let layout = collectionView.collectionViewLayout as? UICollectionViewFlowLayout else {
+            fatalError("Unable to retrieve layout!")
+        }
+        
+        layout.sectionInset = UIEdgeInsets(top: insetAmount, left: insetAmount, bottom: insetAmount, right: insetAmount)
+        layout.minimumLineSpacing = .greatestFiniteMagnitude
+        layout.scrollDirection = .horizontal
+        
+    }
 }
