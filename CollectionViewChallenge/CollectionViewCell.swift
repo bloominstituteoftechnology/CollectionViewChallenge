521e70c34783ce0387ea2215540e9dfbea81f556
Audrey Welch <audreywelch@Audreys-MacBook-Pro.local>
2018-11-28 16:08:08 -0600

    Set up the view cell

--- CollectionViewChallenge/CollectionViewCell.swift
+++ CollectionViewChallenge/CollectionViewCell.swift
@@ -2,4 +2,8 @@ import UIKit
 
 class CollectionViewCell: UICollectionViewCell {
     
+    // Static property that can be accessed throughout the class without instantiating a class instance
+    static let reuseIdentifier = "cell"
+    
+    @IBOutlet weak var imageView: UIImageView!
 }
