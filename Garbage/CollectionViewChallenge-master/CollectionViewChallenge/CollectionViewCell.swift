db5556da6c6ad08adcd02c40f2df65e412b3a151
Jaspal Suri <44656000+JaspalSuri@users.noreply.github.com>
2018-11-28 16:07:00 -0800

    removed old project files

--- Garbage/CollectionViewChallenge-master/CollectionViewChallenge/CollectionViewCell.swift
+++
@@ -1,6 +0,0 @@
-import UIKit
-
-class CollectionViewCell: UICollectionViewCell {
-    static let reuseIdentifier = "cell"
-    @IBOutlet weak var imageView: UIImageView!
-}
--- Garbage/Previous Project Files/CollectionViewChallenge/CollectionViewCell.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-class CollectionViewCell: UICollectionViewCell
-// UICollectionReusableView
-{
-    static let reuseIdentifier = "cell"
-    @IBOutlet weak var collectionViewImageCell: UIImageView!
-}
